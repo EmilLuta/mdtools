6002b38ac0c7226c46dce874962c14b3bb1ab193
EmilLuta <virgil.luta@gmail.com>
2019-03-18 22:08:07 +0200

    Exec git command

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Much mdtools so wow")
+	cmd := exec.Command("git", "log")
+	gitOutput, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Git Error: %s\n", err)
+	}
+	fmt.Println(string(gitOutput))
 }
